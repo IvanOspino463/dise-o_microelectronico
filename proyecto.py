5923f2ad44d5cb8f7c817c9761ea6b427d8b0c00
kevin-cueva <unimagneill@gmail.com>
2022-10-12 21:53:21 -0500

    Login implementado

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import re
+from flask import Flask, render_template,request, redirect, url_for, flash
 
 
 app = Flask(__name__)
@@ -7,6 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Este es un mensaje'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        password = request.form['password']
+        mail = request.form['mail']
+        return render_template('login.html')
+        
+
+
 
 
 if __name__ == '__main__':
